7013720c68c9d9d5bd33b2383553831cd74d582a
TomOrdina <55082648+TomOrdina@users.noreply.github.com>
2019-10-23 13:59:14 +0200

    update project

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -6,6 +6,14 @@ sap.ui.define([
 	return Controller.extend("test.test.controller.Home", {
 		onInit: function () {
 
+		},
+		onSnapshot: function (oEvent) {
+			// The image is inside oEvent, on the image parameter,
+			// let's grab it.
+			var sSnapshot = oEvent.getParameter("image");
+			// Do something with it!
+			// As you see in the demo, you can attach it directly to a src of an Image. 
+			// Because it is already a text string it is also easy to POST to a server inside a json message. 
 		}
 	});
 });
